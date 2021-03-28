18589212c29f42421783472e53ec746a1f050a91
LucasGarcez <lucasgarcezdev@gmail.com>
2021-03-28 15:48:32 -0300

    fix typo

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,10 +4,10 @@ export type AuthData = {
   name: string;
 };
 const signIn = (email: string, _password: string): Promise<AuthData> => {
-  // this is a mock of a API, in a real app
-  // will be need connect to some real API,
+  // this is a mock of an API call, in a real app
+  // will be need connect with some real API,
   // send email and password, and if credential is corret
-  //the api will resolve with some token and another datas like the below
+  //the API will resolve with some token and another datas as the below
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
