5c5950a7490460223b313616a41852d85579571e
LucasGarcez <lucasgarcezdev@gmail.com>
2021-03-28 14:50:11 -0300

    document Auth with comments

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -10,42 +10,62 @@ type AuthContextData = {
   signOut(): void;
 };
 
+//Create the Auth Context with the data type specified
+//and a empty object
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({children}) => {
   const [authData, setAuthData] = useState<AuthData>();
+  //the AuthContext init with loading equals true
+  //and stay like this, ultil the data be load from Async Storage
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    //Every time the App is opened, this provider is rendered
+    //and call de loadStorage function.
     loadStorageData();
   }, []);
 
   async function loadStorageData(): Promise<void> {
     try {
+      //Try get the data from Async Storage
       const authDataSerialized = await AsyncStorage.getItem('@AuthData');
       if (authDataSerialized) {
+        //If there are data, it's converted to an Object and the state is updated.
         const _authData: AuthData = JSON.parse(authDataSerialized);
         setAuthData(_authData);
       }
     } catch (error) {
     } finally {
+      //loading finished
       setLoading(false);
     }
   }
 
   const signIn = async () => {
+    //call the service passing credential (email and password).
+    //In a real App this data will be provided by the user from some InputText components.
     const _authData = await authService.signIn(
       'lucasgarcez@email.com',
       '123456',
     );
 
-    AsyncStorage.setItem('@AuthData', JSON.stringify(_authData));
+    //that the user is authenticated and can access the content.
     setAuthData(_authData);
+
+    //Persist the data in the Async Storage
+    //to be recovered in the next user session.
+    AsyncStorage.setItem('@AuthData', JSON.stringify(_authData));
   };
 
   const signOut = async () => {
-    await AsyncStorage.removeItem('@AuthData');
+    //Remove data from context, so the App can be notified
+    //and send the user to the AuthStack
     setAuthData(undefined);
+
+    //Remove the data from Async Storage
+    //to NOT be recoverede in next session.
+    await AsyncStorage.removeItem('@AuthData');
   };
 
   return (
@@ -55,6 +75,8 @@ const AuthProvider: React.FC = ({children}) => {
   );
 };
 
+//A simple hooks to facilitate the access to the AuthContext
+// and permit components to subscribe to AuthContext updates
 function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
